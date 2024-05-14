a624105487ae00a118fb0d9837205db6e9319488
souvick <souvickmazumdar123@gmail.com>
2024-05-14 13:06:55 +0530

    04_Chai with react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,13 @@
 
 import Chai from "./chai"
 function App() {
-  
+  const username="chai aur code"
 
   return (
    <>
-   <h1>CHAI AUR REACT WITH VITE</h1>
+   <h1>CHAI AUR REACT  {username}</h1>
+   {/* inside {} we have to write only evaluated expression */}
+   <p>test para</p>
    <Chai />
    </>
   )
